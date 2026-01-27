b66c78731cb7137d6b5e0903892a663a8fc3fbeb
s-flegg <161213652+s-flegg@users.noreply.github.com>
2026-01-27 10:42:37 +0000

    session1 task2

--- session_1/2_books/tasks.sql
+++ session_1/2_books/tasks.sql
@@ -1,5 +1,5 @@
 -- Enable readable output format
-.mode columns
+.mode table
 .headers on
 
 -- Instructions for students:
@@ -9,5 +9,13 @@
 
 
 -- write your sql code here
+-- SELECT * from books;
 
-SELECT * from books;
+SELECT Title FROM books ORDER BY rating DESC LIMIT 5;
+SELECT Title FROM books ORDER BY published_year DESC LIMIT 3;
+SELECT Title FROM books ORDER BY pages ASC LIMIT 10;
+SELECT Title FROM books WHERE published_year > 2010 ORDER BY rating DESC LIMIT 5; -- none after 2010
+SELECT Title FROM books WHERE pages > 500 AND genre = 'Fantasy' ORDER BY rating DESC;
+SELECT Title FROM books WHERE published_year < 1980 AND pages > 300 ORDER BY published_year;
+SELECT Title FROM books WHERE genre = 'Mystery' ORDER BY rating ASC LIMIT 5;
+SELECT Title FROM books WHERE genre = 'Science Fiction' AND published_year > 2000 ORDER BY published_year LIMIT 5; -- none after 2000
